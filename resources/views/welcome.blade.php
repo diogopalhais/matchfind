f651ff0d7d5c68a444f1b140290ffcecee7d8fee
mailcube <mailcube@mailcubes-MacBook-Pro.local>
2018-04-24 18:23:33 +0100

    ga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,5 +119,15 @@
 
             </div>
         </div>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118140698-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-118140698-1');
+</script>
+
     </body>
 </html>
